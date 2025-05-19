08b6d435ab7d8b134d2f6b5d6c4e1b9bf1c0ca7c
MT1607 <nguyengiason5@gmail.com>
2025-05-20 00:03:15 +0700

    update change icon and logo

--- browser-extensions-manager-ui-main/script/index.js
+++ browser-extensions-manager-ui-main/script/index.js
@@ -2,6 +2,41 @@
 function toggleTheme() {
   const body = document.body;
   const currentTheme = body.getAttribute("data-theme");
+
   body.setAttribute("data-theme", currentTheme === "dark" ? "light" : "dark");
+  changeMainLogo();
+  changeThemeBtnIcon();
+}
+
+function changeMainLogo() {
+  const logo = document.getElementById("logo");
+
+  if (!logo) {
+    return;
+  }
+  
+  const currentSrc = logo.src;
+
+  if (currentSrc.includes("assets/images/logo-light.svg")) {
+    logo.src = "assets/images/logo-dark.svg";
+  } else if (currentSrc.includes("assets/images/logo-dark.svg")) {
+    logo.src = "assets/images/logo-light.svg";
+  }
+}
+
+function changeThemeBtnIcon() {
+  const icon = document.getElementById("icon-theme");
+
+  if (!icon) {
+    return;
+  }
+  
+  const currentSrc = icon.src;
+
+  if (currentSrc.includes("assets/images/icon-sun.svg")) {
+    icon.src = "assets/images/icon-moon.svg";
+  } else if (currentSrc.includes("assets/images/icon-sun.svg")) {
+    icon.src = "assets/images/icon-moon.svg";
+  }
 }
   
